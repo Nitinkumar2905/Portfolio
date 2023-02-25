109db8f81507fd84e0df85da10f606959020de72
nitin <nimble2905@gmail.com>
2023-02-26 01:30:39 +0530

    script.js

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
     $(".hamburger").click(function(){
       $(this).toggleClass("is-active");
     });
+
+    // $(".myDiv").click(function(){
+    //     $(this).toggleClass("mobile-menu")
+    // })
   });
+
+  let x = document.querySelector(".MyDiv");
+  function myFunction() {
+    x.classList.toggle("mobile-menu");
+  //   x.classList.toggle("answer")
+  
+    }
